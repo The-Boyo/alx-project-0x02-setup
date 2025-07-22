0f2c9ab6282b9868b54a0516eb56aa364f2965f0
Eliud Metto <eliudmett093@gmail.com>
2025-07-22 22:54:44 +0300

    Implemented a reusable Card component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,16 @@
+import Card from "@/components/common/Card";
 import React from "react";
 
 const Home = () => {
 	return (
 		<div>
 			<h1>Home Page</h1>
+			<div className="grid grid-cols-1 space-x-1.5 sm:grid-cols-2 md:grid-cols-4">
+				<Card title="Football" content="Chan 2024" />
+				<Card title="Football" content="Chan 2024" />
+				<Card title="Football" content="Chan 2024" />
+				<Card title="Football" content="Chan 2024" />
+			</div>
 		</div>
 	);
 };
