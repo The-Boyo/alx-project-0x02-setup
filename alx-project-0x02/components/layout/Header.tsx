4b45309dded7aacb49992b1b5a9f7c68cc02dd3a
Eliud Metto <eliudmett093@gmail.com>
2025-07-22 17:40:56 +0300

    Added two new routing files 'about.tsx and home.tsx'

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-	return <header>The Header</header>;
+	return (
+		<header className="flex justify-start space-x-2  w-full m-2 p-2 shadow-2xl shadow-amber-500 rounded-md">
+			<p>
+				<Link href={`/home`}>Home</Link>
+			</p>
+			<p>
+				<Link href={`/about`}>About</Link>
+			</p>
+		</header>
+	);
 };
 
 export default Header;
