85e4a183dbdfa2571f20492191061e305b7f6580
Eliud Metto <eliudmett093@gmail.com>
2025-07-24 22:52:16 +0300

    Added a new reusable Button component

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ size, shape }) => {
+	const renderSize = (theSize: string): string => {
+		if (theSize === "small") {
+			return "h-[2em] w-[4em] text-sm";
+		} else if (theSize === "medium") {
+			return "h-[2.5em] w-[5em] text-md";
+		} else if (theSize === "large") {
+			return "h-[3em] w-[7em] text-lg";
+		} else {
+			return "";
+		}
+	};
+
+	return (
+		<button className={`${renderSize(size)} rounded-${shape} text-white m-3`}>
+			{size}
+		</button>
+	);
+};
+
+export default Button;
